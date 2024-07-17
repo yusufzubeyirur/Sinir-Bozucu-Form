2f28e709772af43ac189ec130385570e429be7b5
yusufzubeyirur <yusufzubeyirur@gmail.com>
2024-07-18 01:54:02 +0300

    SinirBozucuForm

--- src/App.js
+++ src/App.js
@@ -1,73 +1,73 @@
-import './styles.css'
-import { useState, useEffect } from 'react'
+import "./styles.css";
+import { useState, useEffect } from "react";
 
 export default function App() {
   function handleChange(event) {
-    const { name, type, value, checked } = event.target
+    const { name, type, value, checked } = event.target;
 
     function hijackResults() {
-      let string = ''
+      let string = "";
 
-      if (name === 'firstName') {
-        string = 'Namık'
-      } else if (name === 'lastName') {
-        string = 'Korona'
+      if (name === "firstName") {
+        string = "Namık";
+      } else if (name === "lastName") {
+        string = "Korona";
       } else {
-        string = 'NamıkKorona@gmail.com'
+        string = "NamıkKorona@gmail.com";
       }
 
-      return string.slice(0, value.length)
+      return string.slice(0, value.length);
     }
 
     function switcharoo(event) {
-      if (value === 'no' && formData.privacyResponse === 'yes') {
-        return 'absolutely'
-      } else if (value === 'no' && formData.privacyResponse === 'absolutely') {
-        return 'yes'
+      if (value === "no" && formData.privacyResponse === "yes") {
+        return "absolutely";
+      } else if (value === "no" && formData.privacyResponse === "absolutely") {
+        return "yes";
       } else {
-        return value
+        return value;
       }
     }
 
-    let dataToRecord
+    let dataToRecord;
 
-    if (name === 'firstName' || name === 'lastName' || name === 'email') {
-      dataToRecord = hijackResults()
-    } else if (type === 'radio') {
-      dataToRecord = switcharoo(event)
+    if (name === "firstName" || name === "lastName" || name === "email") {
+      dataToRecord = hijackResults();
+    } else if (type === "radio") {
+      dataToRecord = switcharoo(event);
     } else {
-      dataToRecord = type === 'checkbox' ? checked : value
+      dataToRecord = type === "checkbox" ? checked : value;
     }
 
     setFormData((prevFormData) => {
       return {
         ...prevFormData,
         [name]: dataToRecord,
-      }
-    })
+      };
+    });
   }
 
   function fakeSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     setFormData((prev) => ({
       ...formData,
       wantsToSubmit: !prev.wantsToSubmit,
-    }))
+    }));
   }
 
   useEffect(() => {
-    if (formData.rating !== '10') {
+    if (formData.rating !== "10") {
       setTimeout(() => {
-        setFormData({ ...formData, rating: '10' })
-      }, 2000)
+        setFormData({ ...formData, rating: "10" });
+      }, 2000);
     }
 
     if (!formData.marketingResponse) {
       setTimeout(() => {
-        setFormData({ ...formData, rating: '10', marketingResponse: true })
-      }, 2000)
+        setFormData({ ...formData, rating: "10", marketingResponse: true });
+      }, 2000);
     }
-  })
+  });
 
   /* 
 	🚨 Not: Yukarıdaki kodların hiçbiri challenge ile doğrudan ilgili değildir. Hiçbiri değiştirilmemelidir.
@@ -102,122 +102,136 @@ export default function App() {
 		2. Gerçek bilgilerinizi girmeye çalışarak ve soruları kullanıcıların normalde yapacağı şekilde yanıtlayarak kodunuzu test edin. Önceki görevi doğru bir şekilde tamamladıysanız, 71. satırın üzerindeki kodlar sayesinde tüm yanıtlarınız mizahi bir şekilde engellenmelidir. 
 	
 	İpucu: Formla etkileşim halindeyken, formla ne *olması gerektiğini* (ve form kontrol edilseydi ne olacağını) anlamak istiyorsanız: console.log(formData)
+
+  n/a = not applicable => bu mantık ıstedıgı satırlarda uygulanamaz oldugu icin belirtildi ayık ol.
 */
 
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    privacyResponse: 'yes',
-    rating: '10',
+    firstName: "",
+    lastName: "",
+    email: "",
+    privacyResponse: "yes",
+    rating: "10",
     marketingResponse: true,
     wantsToSubmit: false,
-  })
+  });
 
   return (
     <form onSubmit={fakeSubmit}>
       <h1>Dünyanın En Sinir Bozucu Formu</h1>
 
       <input
-        type='text'
-        placeholder='Adı'
+        type="text"
+        placeholder="Adı"
         onChange={handleChange}
-        name='firstName'
+        name="firstName"
+        value={formData.firstName}
       />
 
       <input
-        type='text'
-        placeholder='Soyadı'
+        type="text"
+        placeholder="Soyadı"
         onChange={handleChange}
-        name='lastName'
+        name="lastName"
+        value={formData.lastName}
       />
 
       <input
-        type='email'
-        placeholder='Email'
+        type="email"
+        placeholder="Email"
         onChange={handleChange}
-        name='email'
+        name="email"
+        value={formData.email}
       />
 
       <fieldset>
         <legend>
           Gizlilikle ilgili tüm haklarınızdan feragat etmek ister misiniz?
         </legend>
-        <div className='privacy-container'>
+        <div className="privacy-container">
           <label>
             <input
-              type='radio'
-              id='yes'
-              name='privacyResponse'
-              value='yes'
+              type="radio"
+              id="yes"
+              name="privacyResponse"
+              value="yes"
               onChange={handleChange}
+              checked={formData.privacyResponse === "yes"}
             />
             Evet
           </label>
 
           <label>
             <input
-              type='radio'
-              id='no'
-              name='privacyResponse'
-              value='no'
+              type="radio"
+              id="no"
+              name="privacyResponse"
+              value="no"
               defaultChecked
               onChange={handleChange}
+              checked={formData.privacyResponse === "no"}
             />
             Hayır
           </label>
 
           <label>
             <input
-              type='radio'
-              id='absolutely'
-              name='privacyResponse'
-              value='absolutely'
+              type="radio"
+              id="absolutely"
+              name="privacyResponse"
+              value="absolutely"
               onChange={handleChange}
+              checked={formData.privacyResponse === "absolutely"}
             />
             Kesinlikle
           </label>
         </div>
       </fieldset>
 
-      <fieldset className='rating-container'>
+      <fieldset className="rating-container">
         <legend>
           Bu formu 1-10 arasında, 1 en kötü ve 10 en iyi olmak üzere nasıl
           değerlendirirsiniz?
         </legend>
 
-        <select onChange={handleChange} name='rating' defaultValue='10'>
-          <option value='1'>1</option>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-          <option value='4'>4</option>
-          <option value='5'>5</option>
-          <option value='6'>6</option>
-          <option value='7'>7</option>
-          <option value='8'>8</option>
-          <option value='9'>9</option>
-          <option value='10'>10</option>
+        <select
+          onChange={handleChange}
+          name="rating"
+          defaultValue="10"
+          value={formData.rating}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
       </fieldset>
 
-      <label className='marketing-label'>
+      <label className="marketing-label">
         <input
-          type='checkbox'
-          name='marketingResponse'
+          type="checkbox"
+          name="marketingResponse"
           onChange={handleChange}
+          checked={formData.marketingResponse}
         />
 
-        <div className='checkmark'></div>
+        <div className="checkmark"></div>
         <span>Günde 20 pazarlama maili almak istiyorum. </span>
       </label>
 
       <button
-        className={formData.wantsToSubmit ? 'move' : ''}
+        className={formData.wantsToSubmit ? "move" : ""}
         onFocus={fakeSubmit}
         onMouseEnter={fakeSubmit}
       >
         Gönder
       </button>
     </form>
-  )
+  );
 }
